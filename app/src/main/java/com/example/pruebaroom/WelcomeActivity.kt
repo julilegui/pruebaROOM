2a34b7ef7a2cacf9a2de996349f18392ea9d806f
julilegui <julilegui@gmail.com>
2022-11-13 09:55:09 -0500

    Huella dogotal SHA-1

--- app/src/main/java/com/example/pruebaroom/WelcomeActivity.kt
+++ app/src/main/java/com/example/pruebaroom/WelcomeActivity.kt
@@ -1,23 +1,64 @@
 package com.example.pruebaroom
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.ActionCodeEmailInfo
+import com.google.firebase.auth.FirebaseAuth
+
+enum class ProviderType{
+
+    BASIC
+
+}
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private var textemail: TextView?=null
+    private var textprovedor: TextView?=null
+    private var btnLogOut: Button?=null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //activity_to_do; antes estaba activity_welcome
         setContentView(R.layout.activity_welcome)
+
+        textemail=findViewById(R.id.textemail)
+        textprovedor=findViewById(R.id.textprovedor)
+        btnLogOut = findViewById(R.id.btnLogOut)
+            val bundle=intent.extras
+        val email = bundle?.getString("email")
+        val provider = bundle?.getString("provider")
+        textemail!!.text=email
+        textprovedor!!.text=provider
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        prefs.putString("email",email)
+        prefs.putString("provider",provider)
+        prefs.apply()
+        btnLogOut?.setOnClickListener {
+            val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+            prefs.clear()
+            prefs.apply()
+            FirebaseAuth.getInstance().signOut()
+
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+
+        }
+
 
         val fab: View = findViewById(R.id.fab)
 
         fab.setOnClickListener{view->
 
             //Acá esta configurado el boton para pasar de WelcomeActivity a
-            Snackbar.make(view,"add", Snackbar.LENGTH_LONG).show()
+            //Snackbar.make(view,"add", Snackbar.LENGTH_LONG).show()
 
             val intent = Intent (this, ToDoActivity::class.java)
             startActivity(intent)
@@ -25,6 +66,20 @@ class WelcomeActivity : AppCompatActivity() {
         }
 
     }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val homeIntent = Intent(this,WelcomeActivity::class.java)
+            .apply {
+                putExtra("email", textemail!!.text.toString())
+                putExtra("provider", textprovedor!!.text.toString())
+            }
+
+        startActivity(homeIntent)
+
+    }
+
+
 
 
 
